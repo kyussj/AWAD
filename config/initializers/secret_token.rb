017fe7db11cd97ae0ce0fb45d8f64a1a46f1331f
Jack Booth <j.booth.07@aberdeen.ac.uk>
2012-04-06 15:19:43 +0100

    First commit for AWAD project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awad::Application.config.secret_token = '6a16b3cc712944daf801c1b3c14847e2ccb4ed6c1216332d6d2e31eb9b888d04cfea368aff97b33aae5d0d8789184d028c7b79f29fd53933730e06e49dd58ab9'
